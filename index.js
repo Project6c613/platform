03bfd488d5ee8d6440693cf80f375f689f157e11
ani6gup <ani6gup@gmail.com>
2017-11-27 14:41:30 -0700

    Added Haunted Mansion HTML

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views/pages'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+
+var cons = require('consolidate');
+
+// view engine setup
+app.engine('html', cons.swig)
+app.set('views', __dirname + '/views/pages');
+app.set('view engine', 'html');
+
+
+// app.set('views', __dirname + '/pages');
+// app.set('view engine', 'ejs');
+
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('home.html');
 });
 
 app.listen(app.get('port'), function() {
