6729b564e680189345cf8c8865a07caf97052585
sfallahadl <sfallaha@asu.edu>
2018-02-11 16:57:47 -0700

    player functions

--- scripts/player.js
+++ scripts/player.js
@@ -19,3 +19,86 @@ var foyerRoom = document.getElementById('u183');
 var kitchenRoom = document.getElementById('u188');
 var diningRoom = document.getElementById('u193');
 var userProfile = document.getElementById('u110');
+var submitAnswerInput = document.getElementById('u225_input')
+var submitButton = document.getElementById('u226')
+
+//keep track of total number of keys
+
+submitButton.onclick = function(room, question, submitAnswerInput)
+{
+	//database data = new database;
+	//var answer = data.room.question.answer;
+
+	/*
+	if(answer == submitAnswerInput)
+	{
+		//show congrats page
+		//disable question
+		//subtract key number
+	}
+	else
+	{
+		//show wrong answer function
+	}
+	*/
+
+}
+
+theaterRoom.onclick = function()
+{
+	//load questions for theater room
+	//id - u204_state0 will get filled with the theater questions
+
+}
+
+arcadeRoom.onclick = function()
+{
+	//load questions for theater room
+	//id - u204_state0 will get filled with the theater questions
+}
+
+libraryRoom.onclick = function()
+{
+	//load questions for theater room
+	//id - u204_state0 will get filled with the theater questions
+
+}
+bedroomRoom.onclick = function()
+{
+	//load questions for theater room
+	//id - u204_state0 will get filled with the theater questions
+}
+
+livingRoom.onclick = function()
+{
+	//load questions for theater room
+	//id - u204_state0 will get filled with the theater questions
+
+}
+
+foyerRoom.onclick = function()
+{
+	//load questions for theater room
+	//id - u204_state0 will get filled with the theater questions
+}
+
+kitchenRoom.onclick = function()
+{
+	//load questions for theater room
+	//id - u204_state0 will get filled with the theater questions
+
+}
+
+diningRoom.onclick = function()
+{
+	//load questions for theater room
+	//id - u204_state0 will get filled with the theater questions
+}
+
+userProfile.onclick = function()
+{
+	//fill user info
+
+}
+
+
