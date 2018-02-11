7f59111d943571e8b0dadc5bd5a796ce246d15be
sfallahadl <sfallaha@asu.edu>
2018-02-11 15:16:33 -0700

    player id's

---
+++ scripts/player.js
@@ -0,0 +1,21 @@
+/*
+	Theater - u158
+	Arcade - u165
+	Library - u168
+	Bedroom - u173
+	Living Room - u178
+	Foyer - u183
+	Kitchen - u188
+	Dining Room - u193
+	User Profile Button - u110
+*/
+
+var theaterRoom = document.getElementById('u158');
+var arcadeRoom = document.getElementById('u165');
+var libraryRoom = document.getElementById('u168');
+var bedroomRoom = document.getElementById('u173');
+var livingRoom = document.getElementById('u178');
+var foyerRoom = document.getElementById('u183');
+var kitchenRoom = document.getElementById('u188');
+var diningRoom = document.getElementById('u193');
+var userProfile = document.getElementById('u110');
