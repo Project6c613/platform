4b6edbae631176e92a018795a826f17dec69330b
Anisha Gupta <ani6gup@gmail.com>
2018-02-19 13:25:00 -0700

    Leaderboard Configured

--- views/pages/data/facebookworkshop.js
+++ views/pages/data/facebookworkshop.js
@@ -4,6 +4,7 @@ var oldScore, newScore;
 var signOutButton;
 var currentUID;
 var nameString;
+var listeningFirebaseRefs = [];
 
 var config = {
   apiKey: "AIzaSyAtlo66pxNfHMxMVwL7MXNkblK1lanJgk4",
@@ -16,6 +17,44 @@ var config = {
 
 firebase.initializeApp(config);
 
+function databaseQueries() {
+  var myUserId = firebase.auth().currentUser.uid;
+  var topParticipantsRef = firebase.database().ref('/users/participants/');
+
+  var fetchUpdates = function(postsRef) {
+    postsRef.on('child_added', function(data) {
+      console.log("Child Added");
+      console.log("Data Key " + data.key);
+      console.log("Data Username " + data.val().username);
+      console.log("Data Score " + data.val().userScore);
+    });
+    postsRef.on('child_changed', function(data) {
+      console.log("Child Changed");
+      console.log("Data Key " + data.key);
+      console.log("Data Username " + data.val().username);
+      console.log("Data Score " + data.val().userScore);
+
+      javascript:(function() {
+        $axure('@leaderboardrepeater').addRepeaterData([
+          {
+            username: {type: 'text', text: data.val().username},
+            score: {type: 'text', text: data.val().userScore},
+          }
+        ]).refreshRepeater();
+      })();
+    });
+    postsRef.on('child_removed', function(data) {
+      console.log("Child Removed");
+    });
+  }
+
+  // Fetching and displaying all posts of each sections.
+  fetchUpdates(topParticipantsRef);
+
+  // Keep track of all Firebase refs we are listening to.
+  listeningFirebaseRefs.push(topParticipantsRef);
+}
+
 
 function onAuthStateChanged(user) {
   // We ignore token refresh events.
@@ -25,6 +64,7 @@ function onAuthStateChanged(user) {
 
   if (user) {
     currentUID = user.uid;
+    databaseQueries();
     console.log(currentUID);
   } else {
     // Set currentUID to null.
@@ -33,34 +73,24 @@ function onAuthStateChanged(user) {
   }
 }
 
-function addToLeaderboardDatabase() {
-  user = firebase.auth().currentUser;
-  var users = firebase.database().ref('/users/');
-  users.child("participants").once('value', getUserScore);
-  function getUserScore(snapshot)
-  {
-    console.log(snapshot.val());
-    snapshot.forEach(userSnapshot => {
-       name = userSnapshot.val().username;
-       score = userSnapshot.val().userScore;
-    });
-    var leaderboardData = {
-      username: name,
-      userScore: score,
-    };
-  }
+// function addToLeaderboardDatabase() {
+//   user = firebase.auth().currentUser;
+//   var users = firebase.database().ref('/users/participants/' + userId);
+//   users.child("participants").once('value', getUserScore);
+//
+//     var leaderboardData = {
+//       username: name,
+//       userScore: score,
+//     };
+//   }
+//
+//   // Write the new post's data simultaneously in the posts list and the user's post list.
+//   var updates = {};
+//   updates['/leaderboard/' + user.uid] = leaderboardData;
+//
+//   return firebase.database().ref().update(updates);
+// }
 
-  // Write the new post's data simultaneously in the posts list and the user's post list.
-  var updates = {};
-  updates['/leaderboard/' + user.uid] = leaderboardData;
-
-  return firebase.database().ref().update(updates);
-
-}
-
-/**
- * Starts listening for new posts and populates posts lists.
- */
 
 function updateLeaderboard(){
   //Leaderboard
